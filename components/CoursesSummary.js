f972a3ac76703f4bc8dd1cb7bf263d3f1d1b8887
Mustafa VURAL <95749154+mustafavurall@users.noreply.github.com>
2024-03-31 20:15:31 +0300

    Toplam Fiyatı Hesap Edebilmesi ve Tasarım

--- components/CoursesSummary.js
+++ components/CoursesSummary.js
@@ -1,12 +1,44 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export default function CoursesSummary({periodName}) {
-  return (
-    <View>
-      <Text>{periodName}</Text>
+export default function CoursesSummary({periodName,courses}) 
+{
+
+     const coursesSum= courses.reduce((sum,course)=> {
+        return sum + course.amount // değerleri toplayacak
+
+     },0)
+
+
+
+
+  return (//bastırma
+    <View style={styles.container}>
+      <Text style={styles.title}>{periodName}</Text>
+      <Text style={styles.cost}>{coursesSum + '₺'}</Text>
+
     </View>
   )
 }
+//css özellikler
+const styles = StyleSheet.create({
+    container:{
+flexDirection: 'row',
+justifyContent:'space-between',
+alignItems:'center', // dikey hizalama
+backgroundColor:'turquoise',
+padding: 10,
+borderRadius: 14,
 
-const styles = StyleSheet.create({})
+    },
+    title:{
+        color:'white',
+        fontSize: 20,
+    },
+    cost:{
+        color:'blue',
+        fontSize: 25,
+        fontWeigh: 'bold',
+
+    },
+})
