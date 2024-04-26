0eabf2d0e7f9aca112dc4448ecfffde35c27955f
Mustafa VURAL <95749154+mustafavurall@users.noreply.github.com>
2024-04-26 03:45:04 +0300

    Duruma göre Kurs Ekleme Veya Güncelleme

--- ManageCourse.js
+++ ManageCourse.js
@@ -1,7 +1,23 @@
 import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 
-export default function ManageCourse() {
+
+
+export default function ManageCourse({route, navigation}) {
+  const courseId=route.params?.courseId;
+  let isEditing = false;
+  
+  if(courseId)
+  {
+    isEditing=true
+  }
+  
+  useLayoutEffect(()=>{
+    navigation.setOptions({
+      title: isEditing?'Kursu Güncelle':'Kurs Ekle',
+    })
+
+  },[navigation,isEditing]);
   return (
     <View>
       <Text>ManageCourse</Text>
