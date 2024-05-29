0201307f4f72365c68c259c7503ef4440a4619ba
Mustafa VURAL <95749154+mustafavurall@users.noreply.github.com>
2024-05-29 16:23:51 +0300

    FireBase'den Kursları Çekme İşlemleri

--- RecentCourses.js
+++ RecentCourses.js
@@ -1,8 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React, { useContext } from 'react'
+import React, { useEffect } from 'react'
 import Courses from '../components/Courses';
 import { CoursesContext } from '../store/coursesContext';
 import { getLastWeek } from '../helper/date';
+import { useContext } from 'react';
+import { getCourses } from '../helper/http';
 
 
 
@@ -11,6 +13,19 @@ export default function RecentCourses() {
 
     const coursesContext = useContext(CoursesContext);
     //Son Tarihi Listeleme
+  
+  
+  
+  useEffect(()=>{
+    async function takeCourses(){
+        const courses= await getCourses();
+
+    }
+
+   takeCourses();
+  });
+  
+  
     const RecentCourses=coursesContext.courses.filter((course)=>{
 const today=new Date();
 const dateLastWeek=getLastWeek(today,7);
